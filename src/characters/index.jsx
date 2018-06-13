8f5a2042b6e1625a0f7775de1ed922320f718882
Ignacio Tolstoy <arkantos798@gmail.com>
2018-06-13 20:03:12 +0200

    Home page with character cards

--- src/characters/index.jsx
+++ src/characters/index.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+
+import Grid from '@material-ui/core/Grid';
+
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
 
 import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
 
+import data from './sample';
+
 const styles = theme => ({
+  card: {
+    height: 200,
+    width: 150
+  },
+  divider: {
+    'margin-bottom': theme.spacing.unit
+  },
   button: {
     position: 'absolute',
     bottom: theme.spacing.unit * 2,
@@ -14,14 +30,34 @@ const styles = theme => ({
 });
 
 const Characters = props => (
-  <Button
-    variant="fab"
-    color="primary"
-    aria-label="add"
-    className={props.classes.button}
-  >
-    <AddIcon />
-  </Button>
+  <Fragment>
+    <Grid container justify="center" spacing={16}>
+      {data.map((character, key) => (
+        <Grid key={key} item>
+          <Card className={props.classes.card}>
+            <CardContent>
+              <Typography variant="headline" align="center" component="h2">
+                {character.name}
+              </Typography>
+
+              <Divider className={props.classes.divider} />
+
+              <Typography variant="caption">{character.description}</Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
+
+    <Button
+      variant="fab"
+      color="primary"
+      aria-label="add"
+      className={props.classes.button}
+    >
+      <AddIcon />
+    </Button>
+  </Fragment>
 );
 
 Characters.propTypes = {
---
+++ src/characters/sample.jsx
@@ -0,0 +1,27 @@
+export default [
+  {
+    name: 'Geucha',
+    description:
+      'A short description of what is this about ... more text to make this wrap! A short description of what is this about ... more text to make this wrap!'
+  },
+  {
+    name: 'Jacko',
+    description:
+      'A short description of what is this about ... more text to make this wrap! A short description of what is this about ... more text to make this wrap!'
+  },
+  {
+    name: 'Sakura',
+    description:
+      'A short description of what is this about ... more text to make this wrap! A short description of what is this about ... more text to make this wrap!'
+  },
+  {
+    name: 'Wally',
+    description:
+      'A short description of what is this about ... more text to make this wrap! A short description of what is this about ... more text to make this wrap!'
+  },
+  {
+    name: 'Saphi',
+    description:
+      'A short description of what is this about ... more text to make this wrap! A short description of what is this about ... more text to make this wrap!'
+  }
+];
