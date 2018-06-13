e09d60f5cc9ba594595d47afc024ce2de5a5a5d3
Ignacio Tolstoy <arkantos798@gmail.com>
2018-06-13 20:14:45 +0200

    Fix the position of the create button

--- src/characters/index.jsx
+++ src/characters/index.jsx
@@ -23,7 +23,7 @@ const styles = theme => ({
     'margin-bottom': theme.spacing.unit
   },
   button: {
-    position: 'absolute',
+    position: 'fixed',
     bottom: theme.spacing.unit * 2,
     right: theme.spacing.unit * 2
   }
